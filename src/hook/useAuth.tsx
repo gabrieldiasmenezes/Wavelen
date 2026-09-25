c48f8d85421d9b320d95f6928ebdb8509521ee19
gabrieldiasmenezes <g.dias120306@gmail.com>
2026-09-24 21:35:04 -0300

    fix: validate all application contexts

--- src/hook/useAuth.tsx
+++ src/hook/useAuth.tsx
@@ -3,5 +3,9 @@ import { AuthContext } from "../context/auth/authContext";
 
 export default function useAuth(){
     const context=useContext(AuthContext)
+    if (!context) {
+        throw new Error("useAuth must be used inside AuthProvider");
+    }
+
     return context
 }
--- src/hook/useOnboarding.tsx
+++ src/hook/useOnboarding.tsx
@@ -3,5 +3,9 @@ import { OnboardingContext } from "../context/Onboarding/onboardingContext";
 
 export default function useOnboarding(){
     const context=useContext(OnboardingContext)
+    if (!context) {
+        throw new Error("useAuth must be used inside AuthProvider");
+    }
+
     return context
 }
